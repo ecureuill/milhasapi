4af233fbe0982084bec08ae2f995f83eee256858
Camilla <logikasciuro@gmail.com>
2023-07-27 00:09:38 +0000

    🎉  Begin project

---
+++ src/main/java/ecureuill/milhasapi/MilhasapiApplication.java
@@ -0,0 +1,13 @@
+package ecureuill.milhasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilhasapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilhasapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ecureuill/milhasapi/MilhasapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ecureuill.milhasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilhasapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
