2ab5a5b7a858dfd8b53eb95785f4ab0e69223886
Camilla <logikasciuro@gmail.com>
2023-07-27 00:30:45 +0000

    🚧 (TestimonialRepository) create repository

---
+++ src/main/java/ecureuill/milhasapi/domain/testimonial/TestimonialRepository.java
@@ -0,0 +1,7 @@
+package ecureuill.milhasapi.domain.testimonial;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestimonialRepository extends JpaRepository<Testimonial, Long>{
+    
+}
