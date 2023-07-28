aa4a16f26ccd6bb6941fe9ae3f5c88c93c3c2e4f
Camilla <logikasciuro@gmail.com>
2023-07-28 14:20:36 +0000

    🛂 Enable CORS to all origins, methods and headers

---
+++ src/main/java/ecureuill/milhasapi/infra/security/CorsConfig.java
@@ -0,0 +1,17 @@
+package ecureuill.milhasapi.infra.security;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                      .allowedOrigins("*")
+                      .allowedMethods("*")
+                      .allowedHeaders("*")
+                      .allowCredentials(true);
+    }
+    
+}
